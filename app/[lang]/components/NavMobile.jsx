5ee1135855566148112e2c78b6aaad09568f7767
MahmoudKSultan <mahmoodkns@hotmail.com>
2023-09-14 15:50:54 +0300

    fix mobile menu..

--- app/[lang]/components/NavMobile.jsx
+++ app/[lang]/components/NavMobile.jsx
@@ -1,40 +1,59 @@
-import React from 'react'
+'use client'
+import Link from 'next/link'
+import React, { useState } from 'react'
 
 // import icons
-import { Link } from 'react-router-dom';
-import { AiOutlineClose } from 'react-icons/ai';
-import { FaBars } from 'react-icons/fa';
-// FaBars 
+import { AiOutlineClose } from 'react-icons/ai'
+import { FaBars } from 'react-icons/fa'
+
+// FaBars
 const NavMobile = () => {
-  // const [isOpen, setisOpen] = useState(false);
+  const [isOpen, setisOpen] = useState(false);
+  console.log(isOpen);
   return (
-    <nav className='lg:hidden relative z-20'>
+    <nav className='relative z-20 lg:hidden'>
       {/* menu icons */}
-      <button >
-        <span className='content-none absolute -top-1 -right-1 w-4 h-4 bg-[#1A173E] p-4 rounded-sm'></span> <FaBars size={22} className="text-[#04C873] relative" />
+      <button type='button' onClick={() => setisOpen(prev => !prev)}>
+        <span className='absolute -right-1 -top-1 h-4 w-4 rounded-sm bg-[#1A173E] p-4 content-none'></span>{' '}
+        <FaBars size={22} className='relative text-[#04C873]' />
       </button>
       {/* nav list */}
-      <ul className={`${isOpen ? 'right-0' : 'right-full'
-        } bg-[#1A173E] fixed top-0 w-full h-screen text-white transition-all flex flex-col
-          justify-center items-center space-y-8 text-lg ` }>
+      <ul
+        className={`${
+          isOpen ? 'right-0' : 'right-full'
+        } fixed top-0 flex h-screen w-full flex-col items-center justify-center space-y-8
+          bg-[#1A173E] text-lg text-white transition-all`}
+      >
         {/* close button */}
-        <button  className='absolute top-6 left-6'>
-          <AiOutlineClose size={22} className="text-[#fff]" />
+        <button className='absolute left-6 top-6'>
+          <AiOutlineClose size={22} className='text-[#fff]' onClick={() => setisOpen(false)} />
         </button>
         <li>
-          <Link className='cursor-pointer' href='/home'> الرئيسية</Link>
+          <Link className='cursor-pointer' href='/'>
+            {' '}
+            الرئيسية
+          </Link>
         </li>
         <li>
-          <Link className='cursor-pointer' href='/privacy'>المعالجين </Link>
+          <Link className='cursor-pointer' href='/privacy'>
+            المعالجين{' '}
+          </Link>
         </li>
         <li>
-          <Link className='cursor-pointer' href='/terms'> كيف تحجز جلستك؟ </Link>
+          <Link className='cursor-pointer' href='/terms'>
+            كيف تحجز جلستك؟
+          </Link>
         </li>
-        <li><button className='text-[#04C873] border border-[#04C873] rounded-[26px] py-3 px-6 text-xl font-medium leading-6
-        hover:bg-[#04C873] hover:text-white' >احجز  &nbsp; الأن</button></li>
+        <li>
+          <button
+            className='rounded-[26px] border border-[#04C873] px-6 py-3 text-xl font-medium leading-6 text-[#04C873]
+        hover:bg-[#04C873] hover:text-white'
+          >
+            احجز &nbsp; الأن
+          </button>
+        </li>
       </ul>
-    </nav >
-
+    </nav>
   )
 }
 

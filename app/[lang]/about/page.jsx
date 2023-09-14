2a5fc9cbc2e11c70cb86935153d704ddee867dc4
eslam.albaik <20182663@student.alazhar.edu.ps>
2023-09-14 13:02:19 +0300

    commintlast

---
+++ app/[lang]/about/page.jsx
@@ -0,0 +1,16 @@
+import { getDictionary } from '@/lib/dictionary'
+
+export default async function About({
+  params: { lang }
+}) {
+  const { page } = await getDictionary(lang)
+
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>{page.about.title}</h1>
+        <p className='text-gray-500'>{page.about.description}</p>
+      </div>
+    </section>
+  )
+}
